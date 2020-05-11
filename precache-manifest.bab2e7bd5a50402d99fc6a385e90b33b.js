06bca60f8faa6bdca34da025933a661cf86d1dfe
a-xalimonov <56163332+a-xalimonov@users.noreply.github.com>
2020-05-12 01:49:34 +0900

    Updates

---
+++ precache-manifest.bab2e7bd5a50402d99fc6a385e90b33b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37062cf35434a1c1ca3c5e5a405337f4",
+    "url": "/a-xalimonov/JS_Homework/index.html"
+  },
+  {
+    "revision": "42dde8c22c272b484a9b",
+    "url": "/a-xalimonov/JS_Homework/static/css/main.79f9d420.chunk.css"
+  },
+  {
+    "revision": "2cc49696dffea6b7adaa",
+    "url": "/a-xalimonov/JS_Homework/static/js/2.38d033cb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/a-xalimonov/JS_Homework/static/js/2.38d033cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42dde8c22c272b484a9b",
+    "url": "/a-xalimonov/JS_Homework/static/js/main.887956d6.chunk.js"
+  },
+  {
+    "revision": "14e73849fe9cc43c72b0",
+    "url": "/a-xalimonov/JS_Homework/static/js/runtime-main.3062d7ac.js"
+  }
+]);
